186213f70696059105427c22c1c37215fee968c1
Zsolt Peller <149466363+zsoci1@users.noreply.github.com>
2025-06-03 13:50:11 +0200

    functions

---
+++ database/db.py
@@ -0,0 +1,10 @@
+# def add_customer():
+
+# def get_customers():
+
+# def get_meals_for_week(customer_id, week_start):
+
+# def update_meal():
+
+# def generate_default_meals_for_week(customer_id, week_start):
+
