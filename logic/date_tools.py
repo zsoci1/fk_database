a792d3ba1ecbc82d6b6b317ba99f69b815548c8b
Zsolt Peller <pellerzsolt19@gmail.com>
2025-06-11 11:59:44 +0200

    calc end date function

---
+++ logic/date_tools.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+# fuggveny ami kiszamolja az elofizetes lejarati datumat a start_date
+# es a duration alapjan, csak a munkanapokat (vas-csut) szamolja
+
+def calc_end_date(start_date, duration):
+
+    start_date = datetime.strptime(start_date,"%Y-%m-%d")
+    working_days = 0
+    current_date = start_date
+
+    while working_days < duration:
+        if current_date.weekday() in [6, 0, 1, 2, 3]: # vasarnap - csut
+            working_days += 1
+        current_date += timedelta(days=1)
+
+    return (current_date - timedelta(days=1)).strftime("%Y-%m-%d")
+    
+print(calc_end_date("2025-06-11", 20)) # testing
