d88bd8672901c47b762ec20b50f0d96f9abfb33d
Zsolt Peller <pellerzsolt19@gmail.com>
2025-06-11 08:22:06 +0200

    con

--- database/db.py
+++ database/db.py
@@ -1,35 +1,14 @@
 import sqlite3
 
-try:
+# establish connection with the databse
+conn = sqlite3.connect('meals.db')
 
-    # connect to DB and create a cursor -> for executing queries
-    sqliteConnection = sqlite3.connect('meals.db')
-    cursor = sqliteConnection.cursor()
-    print("db init")
+# cursor is the middleman between the database and the sqlite commands
+c = conn.cursor()
+print(c)
 
-    # write a query and execute it with a cursor
-    # note: queries can be written in form of a string
-    query = 'select sqlite_version();'
-    cursor.execute(query)
 
-    # fetch and output result
-    result = cursor.fetchall()
-    print('SQlite version is {}'.format(result))
 
-    # close the cursor
-    cursor.close()
-
-# handle errors
-except sqlite3.Error as error:
-    print('Error occured - ', error)
-
-# close DB connection irrespective of success
-# or failure
-finally:
-
-    if sqliteConnection:
-        sqliteConnection.close()
-        print('SQLite connection closed')    
 
 #add customer
     # calculate_end_date() function, 2 parameters: start_date, duration
