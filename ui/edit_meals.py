3843560e8a8d9855b52b66776670a970df358bf3
dop14 <dopeller14@protonmail.com>
2025-06-13 11:10:51 +0200

    Search bar customer info

--- ui/edit_meals.py
+++ ui/edit_meals.py
@@ -10,6 +10,7 @@ class ModPage(ctk.CTkFrame):
         self.change_default()
         self.change_subscription()
         self.change_duration()
+        self.show_table()
 
     def setup_window(self):
         self.grid_columnconfigure(0, weight=0)
@@ -22,10 +23,6 @@ class ModPage(ctk.CTkFrame):
         self.search_entry.grid(row =1, column=0, padx=(20,0), pady=10, sticky="w")
         # After every keyrelease, update the suggestions
         self.search_entry.bind("<KeyRelease>", self.update_suggestions)
-        
-        self.load_button = ctk.CTkButton(self, text="Keresés", font=("Arial", 12), width=15, command=self.delete_suggestions)
-        self.load_button.grid(row=1, column=0,padx=(165,0), pady=10, sticky="w")
-
         self.suggestion_frame = None
 
     # Get back live query results
@@ -33,7 +30,7 @@ class ModPage(ctk.CTkFrame):
 
         # Get input from entry
         query = self.search_entry.get().strip()
-
+        
         # If entry is empty
         if query == "":
             # If frame exists, destroy it and set it to none
@@ -53,29 +50,52 @@ class ModPage(ctk.CTkFrame):
 
         # Search in database
         matches = search_customers(query)
+        print(matches)
+
+        # If no matches
+        if not matches:
+            if self.suggestion_frame:
+                self.suggestion_frame.destroy()
+                self.suggestion_frame = None
+
 
         # Format matches
         names = [name for (_, name, _) in matches]
 
+        phones = [phone for (_, _, phone) in matches]
+
         # Show maximum 5 names
         for i,name in enumerate(names[:5]): 
             btn = ctk.CTkButton(self.suggestion_frame, text=name, 
-                                           command=lambda n=name: self.select_customer(n),
+                                           command=lambda n=name: self.select_customer(n, phones),
                                            fg_color="lightgray", text_color="black", corner_radius=8)
             btn.grid(row=i, column= 0,padx=10, pady=2)
 
-    def select_customer(self, name):
+    def select_customer(self, name, phones):
         self.search_entry.delete(0, 'end')
         self.search_entry.insert(0, name)
-        print(f"Kiválasztott ügyfél: {name}")
         self.chosen_name = name
+        self.chosen_number = phones
+        self.delete_suggestions()
 
     def delete_suggestions(self):
         if self.suggestion_frame:
             self.suggestion_frame.destroy()
             self.suggestion_frame = None
-        print(f"Person chosen: {self.chosen_name}")
+        self.delete_input(self.search_entry)
+        self.show_customer_info()
 
+    def show_customer_info(self):
+        if (hasattr(self, 'customer_name_label') and self.customer_name_label.winfo_exists() 
+            and hasattr(self, 'customer_phone_label') and self.customer_phone_label.winfo_exists()):
+            self.customer_name_label.destroy()
+            self.customer_phone_label.destroy()
+        
+        self.customer_name_label = ctk.CTkLabel(self, text=self.chosen_name, font=("Arial", 18))
+        self.customer_name_label.grid(row=2, column=0, padx=20, sticky="w")
+
+        self.customer_phone_label = ctk.CTkLabel(self, text=self.chosen_number, font=("Arial", 18))
+        self.customer_phone_label.grid(row=3, column=0, padx=20, sticky="w")
 
     def change_default(self):
         # Error handling -> cannot use this button if no person is selected in search bar
@@ -109,4 +129,7 @@ class ModPage(ctk.CTkFrame):
         ['Csütörtök (06-16)','M','reggeli, ebéd, snack, vacsora']]
 
         self.table = CTkTable(self, row=6, column=3, values=self.value)
-        self.table.grid(row=6, column=0, padx=20, pady=20, sticky="nsew")
+        self.table.grid(row=9, column=0, padx=20, pady=20, sticky="nsew")
+
+    def delete_input(self,input):
+        input.delete(0, 'end')
